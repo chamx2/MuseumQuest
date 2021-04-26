d9a4b3abf48d9260c26198fa982ba00974fabd3a
flolewis <55874067+flolewis@users.noreply.github.com>
2021-04-26 17:13:54 +0800

    Created Image Loader, Created Museum Scene, Added Image Manager

---
+++ Assets/Scripts/ImageLoadingManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageLoadingManager : MonoBehaviour
+{
+    public ImageLoader[] loaders;
+    public string[] urls;
+    private void Awake()
+    {
+       loaders = FindObjectsOfType<ImageLoader>();
+        for (int i = 0; i<loaders.Length; i++) {
+            if (urls.Length > i)
+            {
+                loaders[i].imageUrl = urls[i];
+                Debug.Log("HERE");
+            }
+            else {
+                loaders[i].imageUrl = urls[urls.Length-1];
+            }
+        }
+    }
+}
